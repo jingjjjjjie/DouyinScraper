cc63b3a72e62d1e75698918210416835eee0e947
jingjjjjjie <hengzkit@gmail.com>
2024-03-15 18:56:11 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+install_requires=[
+        'selenium==4.0.0',  
+        'beautifulsoup4==4.12.3',
+        'requests==2.31.0'
+    ],
+
+
+setup(
+    name="DouyinScraper",
+    author="Jason Ma",
+    version="1.0",
+    description="Douyin Scraper",
+    keywords=["douyin", "scraper"],
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=install_requires,
+    packages=find_packages("."),
+)
